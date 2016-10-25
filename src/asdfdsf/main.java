49ffaadde96642cb5ef4eebee0d2d666c63e628e
fcu-D0441406 <ian061504@gmail.com>
2016-10-25 22:45:22 +0800

    Update main.java

--- src/asdfdsf/main.java
+++ src/asdfdsf/main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class dec {
 
 	public static void main(String[] args) {
-		dgdgr d1 = new dgdgr();
+		tidda d1 = new tidda();
 		Scanner s1 = new Scanner(System.in);
 		int a = 1;
 		d1.tidda();
